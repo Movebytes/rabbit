34d3cd7179799a9ad17506cb64cc9175b6e5e20e
Ruslan Butyk <Ruslan.Butyk@infopulse.com>
2018-06-13 17:46:03 +0300

    todo: process management

---
+++ Animal/Source/Inc/Process.h
@@ -0,0 +1,99 @@
+/**
+ *  Copyright 2018 Movebytes Group
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#ifndef _AML_PROCESS_H_
+#define _AML_PROCESS_H_
+#include <memory>
+#include "Types.h"
+#include "ProcessManager.h"
+namespace aml {
+class Process;
+class ProcessManager;
+typedef std::shared_ptr<Process> StrongProcessPtr;
+typedef std::weak_ptr<Process> WeakProcessPtr;
+class Process
+{
+	friend class ProcessManager;
+public:
+	enum State
+	{
+		UNINITIALIZED = 0,
+		REMOVED,
+		RUNNING,
+		PAUSED,
+		SUCCEEDED,
+		FAILED,
+		ABORTED
+	};
+private:
+	State m_state;
+	StrongProcessPtr m_pChild;
+	// Change state
+	void SetState(State state)
+	{
+		m_state = state;
+	}
+public:
+	// Default constructor
+	Process();
+	// Default destructor
+	virtual ~Process();
+	// Functions for ending the process
+	inline void Succeed();
+	inline void Fail();
+	// Pause
+	inline void Pause();
+	inline void UnPause();
+	// Accessors
+	State GetState() const 
+	{
+		return m_state;
+	}
+	bool IsAlive() const
+	{
+		return (m_state == RUNNING || m_state == PAUSED);
+	}
+	bool IsDead() const
+	{
+		return (m_state == SUCCEEDED || m_state == FAILED || m_state == ABORTED);
+	}
+	bool IsRemoved() const 
+	{
+		return (m_state == REMOVED);
+	}
+	bool IsPaused() const
+	{
+		return (m_state == PAUSED);
+	}
+	// Child functions
+	inline void AttachChild(StrongProcessPtr pChild);
+	StrongProcessPtr RemoveChild();
+	StrongProcessPtr GetChild() const
+	{
+		return m_pChild;
+	}
+protected:
+	// Have to override in subclass as needed
+	virtual void Init() 
+	{
+		m_state = RUNNING;
+	}
+	virtual void Update(U64 iDelta) = 0;
+	virtual void Success() {}
+	virtual void Fail() {}
+	virtual void Abort() {}
+}; // Process
+} // aml
+#endif // _AML_PROCESS_H_
---
+++ Animal/Source/Inc/Types.h
@@ -0,0 +1,69 @@
+/**
+ *  Copyright 2018 Movebytes Group
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#ifndef _AML_TYPES_H_
+#define _AML_TYPES_H_
+
+#ifndef _AML_SIGNED_LONG_
+#define _AML_SIGNED_LONG_
+typedef long long S64;
+#endif //_AML_SIGNED_LONG_
+
+#ifndef _AML_UNSIGNED_LONG_
+#define _AML_UNSIGNED_LONG_
+typedef unsigned long long U64;
+#endif //_AML_UNSIGNED_LONG_
+
+#ifndef _AML_SIGNED_INT_
+#define _AML_SIGNED_INT_
+typedef int S32;
+#endif //_AML_SIGNED_INT_
+
+#ifndef _AML_UNSIGNED_INT_
+#define _AML_UNSIGNED_INT_
+typedef unsigned int U32;
+#endif //_AML_UNSIGNED_INT_
+
+#ifndef _AML_SIGNED_SHORT_
+#define _AML_SIGNED_SHORT_
+typedef short S16;
+#endif //_AML_SIGN_SHORT_
+
+#ifndef _AML_UNSIGNED_SHORT_
+#define _AML_UNSIGNED_SHORT_
+typedef unsigned short U16;
+#endif //_AML_UNSIGNED_SHORT_
+
+#ifndef _AML_SIGNED_CHAR_
+#define _AML_SIGNED_CHAR_
+typedef char S8;
+#endif // _AML_SIGNED_CHAR_
+
+#ifndef _AML_UNSIGNED_CHAR_
+#define _AML_UNSIGNED_CHAR_
+typedef unsigned char U8;
+#endif //_AML_UNSIGNED_CHAR_
+
+#ifndef _AML_FLOAT_
+#define _AML_FLOAT_
+typedef float F32;
+#endif // _AML_FLOAT_
+
+#ifndef _AML_DOUBLE_
+#define _AML_DOUBLE_
+typedef double F64;
+#endif // _AML_DOUBLE_
+
+#endif //_AML_TYPES_H_
